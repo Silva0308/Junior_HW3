6f91424d13e7532f98a65c275904d57a49b5fe68
Silva0308 <v.travinina@gmail.com>
2023-12-13 19:57:42 +0300

    HW3 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
-
+import java.io.*;
+//Разработайте класс Student с полями String name, int age, transient double GPA (средний балл).
+// Обеспечьте поддержку сериализации для этого класса. Создайте объект класса Student и инициализируйте его данными.
+// Сериализуйте этот объект в файл. Десериализуйте объект обратно в программу из файла.
+// Выведите все поля объекта, включая GPA, и обсудите, почему значение GPA не было сохранено/восстановлено.//
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException  {
+        Student s = new Student("John Smith", 20, 3.5); //Значение GPA не будет сохранено/восстановлено, потому что мы объявили это поле как transient. Это означает, что оно не будет сохраняться при сериализации объекта в файл.
+        s.serializeToFile("student.ser");
+
+        Student deserialized = Student.deserializeFromFile("student.ser");
+        deserialized.printInfo();
+
+}
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,59 @@
-package org.example;public class Student {
+package org.example;
+import java.io.*;
+
+public class Student implements Serializable {
+    private String name;
+    private int age;
+    private transient double GPA; //  Поле GPA объявлено как transient, что означает, что оно не будет сохраняться при сериализации.
+
+    private Student() {
+    }
+
+
+    public Student(String name, int age, double GPA) {
+        this.name = name;
+        this.age = age;
+        this.GPA = GPA;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", GPA=" + GPA +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void serializeToFile(String fileName) throws IOException {
+        FileOutputStream fileOutput = new FileOutputStream(fileName);
+        ObjectOutputStream output = new ObjectOutputStream(fileOutput);
+        output.writeObject(this);
+        output.close();
+        fileOutput.close();
+    }
+
+    public static Student deserializeFromFile(String fileName) throws IOException, ClassNotFoundException {
+        FileInputStream fileInput = new FileInputStream(fileName);
+        ObjectInputStream input = new ObjectInputStream(fileInput);
+        Student s = (Student) input.readObject();
+        input.close();
+        fileInput.close();
+        return s;
+    }
+
+    public void printInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + GPA);
+    }
+
+
 }
